912f6ec36f0205e59eafaaeaed797227fc189bc0
ramyazdi <ramyazdi1012@gmail.com>
2019-08-10 11:40:55 +0300

    update globals.py

--- perturbated_linear_parser/globals.py
+++ perturbated_linear_parser/globals.py
@@ -1,7 +1,7 @@
 import os
 
 DATA = '../../DATA'
-NOISED_LIST_DIR_NAME = 'noised_list_after_predicted_pos_perturbated_model'
-RESULTS_DIR = os.path.join(DATA,'results_cldp_after_predicted_pos')
+NOISED_LIST_DIR_NAME = 'noised_list'
+RESULTS_DIR = os.path.join(DATA,'results')
 UNIVERSAL_DEP_DIR = 'Universal_Dependencies_2.0'
 
